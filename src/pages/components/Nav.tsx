fc83c93959825fa61e2f226daff723960f60b5b2
maxwellyoung <maxtheyoung@gmail.com>
2023-03-21 21:14:34 +1300

    responsiveness improved

--- src/pages/components/Nav.tsx
+++ src/pages/components/Nav.tsx
@@ -20,74 +20,76 @@ export default function Nav() {
             alt="tom crampin logo"
           />
         </Link>
-        <div className={`hamburger ml-auto ${modal ? "hidden" : "block"}`}>
-          <button
-            className="group flex h-12 w-12 flex-col items-center justify-center"
-            onClick={() => openModal(!modal)}
-          >
-            <div
-              className={`${genericHamburgerLineClosed} ${
-                modal
-                  ? "translate-y-3 rotate-45 opacity-50 group-hover:opacity-100"
-                  : "opacity-50 group-hover:opacity-100"
-              }`}
-            />
-            <div
-              className={`${genericHamburgerLineClosed} ${
-                modal ? "opacity-0" : "opacity-50 group-hover:opacity-100"
-              }`}
-            />
-          </button>
-        </div>
+        <button
+          className="group ml-auto flex h-12 w-12 flex-col items-end justify-center"
+          onClick={() => openModal(!modal)}
+        >
+          <div
+            className={`${genericHamburgerLineClosed} ${
+              modal
+                ? "translate-y-3 rotate-45 opacity-50 group-hover:opacity-100"
+                : "opacity-50 group-hover:opacity-100"
+            }`}
+          />
+          <div
+            className={`${genericHamburgerLineClosed} ${
+              modal ? "opacity-0" : "opacity-50 group-hover:opacity-100"
+            }`}
+          />
+        </button>
       </div>
 
       {modal && (
-        <div className="modal fixed top-0 left-0 z-50 flex h-screen w-screen flex-col items-start justify-end gap-4 bg-black p-24 text-left">
-          <button
-            className="group flex h-12 w-12 flex-col items-center justify-center"
-            onClick={() => openModal(!modal)}
-          >
-            <div
-              className={`${genericHamburgerLineOpen} ${
-                modal
-                  ? "translate-y-3 rotate-45 opacity-50 group-hover:opacity-100"
-                  : "opacity-50 group-hover:opacity-100"
-              }`}
-            />
-            <div
-              className={`${genericHamburgerLineOpen} ${
-                modal ? "opacity-0" : "opacity-50 group-hover:opacity-100"
-              }`}
-            />
-            <div
-              className={`${genericHamburgerLineOpen} ${
-                modal
-                  ? "-translate-y-3 -rotate-45 opacity-50 group-hover:opacity-100"
-                  : "opacity-50 group-hover:opacity-100"
-              }`}
-            />
-          </button>
-          <Link
-            href="/work"
-            className="text-8xl font-bold text-[#3A3A3A] hover:text-white sm:text-9xl "
-            onClick={() => openModal(!modal)}
-          >
-            work
-          </Link>
-          <Link
-            href="/about"
-            className="text-8xl font-bold text-[#3A3A3A] hover:text-white sm:text-9xl"
-            onClick={() => openModal(!modal)}
-          >
-            about
-          </Link>
-          <Link
-            href="mailto:tom@tomcrampin.com"
-            className="text-8xl font-bold text-[#3A3A3A] hover:text-white sm:text-9xl"
-            onClick={() => openModal(!modal)}
-          >
-            contact
-          </Link>
+        <div className="modal fixed  z-50 h-screen w-screen items-start justify-end gap-4 bg-black  text-left">
+          <div className="-inset-10 mr-8 ml-auto -mt-8 flex justify-end sm:-mt-2 sm:mr-20">
+            <button
+              className=" group flex h-40 w-40 flex-col items-end justify-center"
+              onClick={() => openModal(!modal)}
+            >
+              <div
+                className={`${genericHamburgerLineOpen} ${
+                  modal
+                    ? "translate-y-3 rotate-45 opacity-50 group-hover:opacity-100"
+                    : "opacity-50 group-hover:opacity-100"
+                }`}
+              />
+              <div
+                className={`${genericHamburgerLineOpen} ${
+                  modal ? "opacity-0" : "opacity-50 group-hover:opacity-100"
+                }`}
+              />
+              <div
+                className={`${genericHamburgerLineOpen} ${
+                  modal
+                    ? "-translate-y-3 -rotate-45 opacity-50 group-hover:opacity-100"
+                    : "opacity-50 group-hover:opacity-100"
+                }`}
+              />
+            </button>
+          </div>
+          <div className="flex flex-col p-12 sm:p-24">
+            <Link
+              href="/work"
+              className="text-8xl font-bold text-[#3A3A3A] hover:text-white sm:text-9xl "
+              onClick={() => openModal(!modal)}
+            >
+              work
+            </Link>
+            <Link
+              href="/about"
+              className="text-8xl font-bold text-[#3A3A3A] hover:text-white sm:text-9xl"
+              onClick={() => openModal(!modal)}
+            >
+              about
+            </Link>
+            <Link
+              href="mailto:tom@tomcrampin.com"
+              className="text-8xl font-bold text-[#3A3A3A] hover:text-white sm:text-9xl"
+              onClick={() => openModal(!modal)}
+            >
+              contact
+            </Link>
+          </div>
         </div>
       )}
     </nav>
--- src/pages/work.tsx
+++ src/pages/work.tsx
@@ -1,6 +1,4 @@
 import Head from "next/head";
-// import styles from "../styles/Home.module.css";
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function reel() {
   return (
@@ -9,10 +7,6 @@ export default function reel() {
         <title>Tom Crampin</title>
         <meta name="description" content="Descriptions" />
         <link rel="icon" href="/logo.svg" />
-        {/* <script
-					src="https://kit.fontawesome.com/7e9f3d367c.js"
-					crossOrigin="anonymous"
-				></script> */}
       </Head>
       <main>
         <div className="bg-video-wrap fixed inset-0 z-[-1]">
@@ -22,8 +16,7 @@ export default function reel() {
             loop
             preload="true"
             autoPlay
-          ></video>
-          {/* <FontAwesomeIcon icon="fa-solid fa-volume-high" /> */}
+          />
         </div>
       </main>
     </div>
