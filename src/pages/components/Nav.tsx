c2edf1ce9a3d9f57789e8bcef658649889030558
maxwellyoung <maxtheyoung@gmail.com>
2023-03-21 23:50:38 +1300

    nav animation

--- src/pages/components/Nav.tsx
+++ src/pages/components/Nav.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function Nav() {
   const [modal, openModal] = useState(false);
@@ -38,60 +39,67 @@ export default function Nav() {
           />
         </button>
       </div>
-
-      {modal && (
-        <div className="modal fixed  z-50 h-screen w-screen items-start justify-end gap-4 bg-black  text-left">
-          <div className="-inset-10 mr-8 ml-auto -mt-8 flex justify-end sm:-mt-2 sm:mr-20">
-            <button
-              className=" group flex h-40 w-40 flex-col items-end justify-center"
-              onClick={() => openModal(!modal)}
-            >
-              <div
-                className={`${genericHamburgerLineOpen} ${
-                  modal
-                    ? "translate-y-3 rotate-45 opacity-50 group-hover:opacity-100"
-                    : "opacity-50 group-hover:opacity-100"
-                }`}
-              />
-              <div
-                className={`${genericHamburgerLineOpen} ${
-                  modal ? "opacity-0" : "opacity-50 group-hover:opacity-100"
-                }`}
-              />
-              <div
-                className={`${genericHamburgerLineOpen} ${
-                  modal
-                    ? "-translate-y-3 -rotate-45 opacity-50 group-hover:opacity-100"
-                    : "opacity-50 group-hover:opacity-100"
-                }`}
-              />
-            </button>
-          </div>
-          <div className="flex h-screen flex-col justify-center p-4 sm:p-24">
-            <Link
-              href="/work"
-              className="text-7xl font-bold text-[#3A3A3A] hover:text-white sm:text-9xl "
-              onClick={() => openModal(!modal)}
-            >
-              work
-            </Link>
-            <Link
-              href="/about"
-              className="text-7xl font-bold text-[#3A3A3A] hover:text-white sm:text-9xl"
-              onClick={() => openModal(!modal)}
-            >
-              about
-            </Link>
-            <Link
-              href="mailto:tom@tomcrampin.com"
-              className="text-7xl font-bold text-[#3A3A3A] hover:text-white sm:text-9xl"
-              onClick={() => openModal(!modal)}
-            >
-              contact
-            </Link>
-          </div>
-        </div>
-      )}
+      <AnimatePresence>
+        {modal && (
+          <motion.div
+            key="modal"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className="modal fixed  z-50 h-screen w-screen items-start justify-end gap-4 bg-black  text-left"
+          >
+            <div className="-inset-10 mr-8 ml-auto -mt-8 flex justify-end sm:-mt-2 sm:mr-20">
+              <button
+                className=" group flex h-40 w-40 flex-col items-end justify-center"
+                onClick={() => openModal(!modal)}
+              >
+                <div
+                  className={`${genericHamburgerLineOpen} ${
+                    modal
+                      ? "translate-y-3 rotate-45 opacity-50 group-hover:opacity-100"
+                      : "opacity-50 group-hover:opacity-100"
+                  }`}
+                />
+                <div
+                  className={`${genericHamburgerLineOpen} ${
+                    modal ? "opacity-0" : "opacity-50 group-hover:opacity-100"
+                  }`}
+                />
+                <div
+                  className={`${genericHamburgerLineOpen} ${
+                    modal
+                      ? "-translate-y-3 -rotate-45 opacity-50 group-hover:opacity-100"
+                      : "opacity-50 group-hover:opacity-100"
+                  }`}
+                />
+              </button>
+            </div>
+            <div className="flex h-screen flex-col justify-center p-4 sm:p-24">
+              <Link
+                href="/work"
+                className="text-7xl font-bold text-[#3A3A3A] hover:text-white sm:text-9xl "
+                onClick={() => openModal(!modal)}
+              >
+                work
+              </Link>
+              <Link
+                href="/about"
+                className="text-7xl font-bold text-[#3A3A3A] hover:text-white sm:text-9xl"
+                onClick={() => openModal(!modal)}
+              >
+                about
+              </Link>
+              <Link
+                href="mailto:tom@tomcrampin.com"
+                className="text-7xl font-bold text-[#3A3A3A] hover:text-white sm:text-9xl"
+                onClick={() => openModal(!modal)}
+              >
+                contact
+              </Link>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </nav>
   );
 }
